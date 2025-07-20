96725c3a4ad13c27735c3941e868570821b5b5e9
Märt Kalmo <mart.kalmo@gmail.com>
2025-07-20 12:48:31 +0300

    lib fixes

--- index.tsx
+++ index.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 
 import AppComp from './comp/AppComp.tsx';
 import { HashRouter } from "react-router-dom";
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         <HashRouter basename='/'>
             <AppComp />
